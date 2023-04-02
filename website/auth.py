f2abbf00a4a862b684fc7956a898e353d844217c
Piyush Kumar Sultania <sultania.p@northeastern.edu>
2023-04-01 21:08:38 -0400

    final build, application working localhost deployment

--- website/auth.py
+++ website/auth.py
@@ -26,7 +26,7 @@ def login():
         else:
             flash('Email does not exist.', category='error')
 
-    return render_template("login.html", boolean= False)
+    return render_template("login.html", user=current_user)
 
 
 
@@ -60,10 +60,10 @@ def sign_up():
             new_user = User(email=email, first_name=first_name, password=generate_password_hash(password1, method='sha256'))
             db.session.add(new_user)
             db.session.commit()
-            login_user(user, remember=True)
+            login_user(new_user, remember=True)
             flash('Account created successfully!', category='success')
             return redirect(url_for('views.home'))
     
-    return render_template("sign_up.html")
+    return render_template("sign_up.html", user=current_user)
 
 
